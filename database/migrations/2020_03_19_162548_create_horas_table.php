0a369d8ad89848cc256fa512c212074fe381a768
fifiuro <fifiuro@gmail.com>
2020-03-19 12:36:47 -0400

    Migraciones Configuracion

---
+++ database/migrations/2020_03_19_162548_create_horas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHorasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horas', function (Blueprint $table) {
+            $table->bigIncrements('id_hrs');
+            // LLAVE FORANEA A LA TABLA AREAS
+            $table->integer('id_are');
+            $table->sting('nivel');
+            $table->integer('horas');
+            $table->string('tiempo');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horas');
+    }
+}
